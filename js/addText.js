c59d411d2a67adbed5f689f05c1f0fb31f38bd49
EvertLagerberg <evert.lagerberg@gmail.com>
2015-03-06 11:08:29 +0100

    Label size depend on area size

--- js/addText.js
+++ js/addText.js
@@ -1,30 +1,36 @@
 function addText(p,name,hektar){
 
-		console.log(hektar);
+		//console.log(hektar);
 		var t = document.createElementNS("http://www.w3.org/2000/svg", "text");
 		var b = p.getBBox();
 		t.setAttribute("transform", "translate(" + (b.x + b.width/2) + " " + (b.y + b.height/2) + ")");
 		t.textContent = name;
 
-		/*if (hektar < 100){
-			t.setAttribute("font-size", "14");
+		if (hektar <= 100){
+			t.setAttribute("font-size", "3");
 
 		}
-		 else if(hektar <250){
+		 else if(hektar > 100 && hektar <= 250){
+		 	console.log(name);
+			t.setAttribute("font-size", "4");
+
+		} else if(hektar > 250 && hektar <=400){
 			t.setAttribute("font-size", "5");
 
-		} else if(hektar>400){
+		}
+		 else if(hektar > 400 && hektar <= 950){
+		 t.setAttribute("font-size", "10");
 
 		}
-		 else if(hektar>250){
-
+		else if(hektar > 950 && hektar <= 1100){
+			t.setAttribute("font-size", "13");
 		}
-		else if hektar>{
-			t.setAttribute("font-size", "5");
+		else{
+		t.setAttribute("font-size", "20");
 		}
-		*/
-		t.setAttribute("font-size", "14");
 		
-		t.setAttribute("fill", "red");
+		
+		
+		t.setAttribute("fill", "black");
 		p.parentNode.insertBefore(t, p.nextSibling);
 };

9872120ed901d981951cb27bb246ab1ce599dfd0
fika4life <fistewie@gmail.com>
2015-03-06 20:25:15 +0100

    adds image generating loop?

--- js/populateInfo.js
+++ js/populateInfo.js
@@ -2,12 +2,16 @@
 
 function populateInfo(id){
 	var index = getStationInfo(id);
-    var images = stationInfoHolder[index]['Images'];
+	//console.log(index);
+    var images = stationInfoHolder[index]['Image'];
 	$('#title').html(stationInfoHolder[index]["Station"]);
 	$('#inaug').html("<strong>Invigdes: </strong>" + stationInfoHolder[index]["Year"]);
 	$('#art').html("<strong>Stationens konstverk: </strong>" + convertSymbols(stationInfoHolder[index]["Art"]));
 	$('#history').html("<strong>Historiska fakta: </strong>" + convertSymbols(stationInfoHolder[index]["Art"]));
 
+	for (var i=0; i<images.length; i++){
+		$('#img-holder ul').html('<li><a href="#"><img src="img/stations/"' + images[i] + ' height="220" alt="" /></a></li>')
+	}
 
 }
 

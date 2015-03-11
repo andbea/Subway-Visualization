15ba5a8d50866e8c38d255182b5efb005d17e855
icolektro5u <smartoboy31@hotmail.com>
2015-03-11 21:16:49 +0100

    Revert "Added panning restriction"
    This reverts commit ec1fcfea0d0c332acb60035f04d187e62d13edfb.

--- js/zoom.js
+++ js/zoom.js
@@ -13,39 +13,7 @@ function zoom() {
 		$(".parishLabel").hide();
 		parishLayer = false;
 	}
-	
-	//Don't allow infinite panning
-	var minXPan = -600 + mapPositionX;
-	var maxXPan = 277 + mapPositionX;
-	var minYPan = -635 + mapPositionY;
-	var maxYPan = 200 + mapPositionY;	
-	var parishLayer = false;
 
 	svg.select("#outer")
-		.attr("transform", function(){	
-			//If map is within boundaries it follows the mouse
-			if (d3.event.translate[0] > minXPan && d3.event.translate[0] < maxXPan && d3.event.translate[1] > minYPan && d3.event.translate[1] < maxYPan ){
-				return "translate(" + d3.event.translate[0]+ "," + d3.event.translate[1] + ")scale(" + mapScale+ ")";
-			}
-			//Otherwise its position will be set to either the min or max
-			else{
-				var panX = d3.event.translate[0];
-				var panY = d3.event.translate[1];
-				
-				if(d3.event.translate[0] <= minXPan ){
-					panX = minXPan;}
-				
-				else if(d3.event.translate[0] >= maxXPan ){
-					panX = maxXPan;}
-				
-				if(d3.event.translate[1] <= minYPan ){
-					panY = minYPan}
-				
-				else if(d3.event.translate[1] >= maxYPan ){
-					panY = maxYPan;}
-				
-				return "translate(" + panX+ "," + (panY) + ")scale(" + mapScale+ ")";
-			}
-		}
-	);
+	.attr("transform", "translate(" + d3.event.translate[0] + "," + d3.event.translate[1] + ")scale(" + d3.event.scale + ")");
 }

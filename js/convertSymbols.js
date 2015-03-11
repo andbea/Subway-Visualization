31ba41110bb8d4da7cfbdfabfd4ea44dee857255
andbea <andbea@kth.se>
2015-03-11 13:50:03 +0100

    Corrected ÅAÖ presentation, again

--- js/convertSymbols.js
+++ js/convertSymbols.js
@@ -1,6 +1,9 @@
 function convertSymbols(string){
-	var newString = string.replace('å', '\&aring');
-	var newString = newString.replace('ä', '\&auml');
-	var newString = newString.replace('ö', '\&ouml');
+	var newString = string.split('å').join('\&aring');
+	var newString = newString.split('ä').join('\&auml');
+	var newString = newString.split('ö').join('\&ouml');
+	var newString = newString.split('Å').join('\&Aring');
+	var newString = newString.split('Ä').join('\&Auml');
+	var newString = newString.split('Ö').join('\&Ouml');
 	return newString;
 }

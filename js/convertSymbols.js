2f7b8dcf0522618e7facb34ee7ffeebda97589e7
andbea <andbea@kth.se>
2015-03-11 12:00:00 +0100

    Fixed the character problem and changed some design

--- js/convertSymbols.js
+++ js/convertSymbols.js
@@ -1,6 +1,6 @@
 function convertSymbols(string){
-	var newString = string.replace('å', '&aring');
-	var newString = newString.replace('ä', '&auml');
-	var newString = newString.replace('ö', '&ouml');
+	var newString = string.replace('å', '\&aring');
+	var newString = newString.replace('ä', '\&auml');
+	var newString = newString.replace('ö', '\&ouml');
 	return newString;
 }
